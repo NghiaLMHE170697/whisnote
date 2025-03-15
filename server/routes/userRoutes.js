243b270c5716a171b8093d240c83a73ccb6dee8f
NghiaLM <Nghialmhe170697@fpt.edu.vn>
2025-03-16 00:31:05 +0700

    Fix backend displaying user profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const { register, login, getProfile, followUser, unfollowUser, verifyOTP } = require("../controllers/userController"); 
+const { register, login, getProfile, verifyOTP } = require("../controllers/userController"); 
 const router = express.Router();
-const { isAuthorized } = require("../middleware/verifyAuth.middleware");
-const User = require("../models/User"); // Ensure the User model is imported
-
 
 
 // Đăng ký & đăng nhập
@@ -12,23 +9,6 @@ router.post("/login", login);
 router.post("/verify-otp", verifyOTP); // Xác nhận OTP & đăng ký
 
 // Xem thông tin người dùng
-router.get("/:id", getProfile);
-router.get("/me", async (req, res) => {
-    try {
-        console.log("🔍 Request userId từ token:", req.userId);
-        
-        const user = await User.findById(req.userId).select("-password");
-        if (!user) return res.status(404).json({ error: "Không tìm thấy người dùng" });
-
-        res.json({ status: "success", data: user });
-    } catch (error) {
-        console.error("🚨 Lỗi khi lấy user:", error);
-        res.status(500).json({ error: "Lỗi server" });
-    }
-});
-
-// Theo dõi / Bỏ theo dõi
-router.post("/:id/follow", followUser);
-router.post("/:id/unfollow", unfollowUser);
+router.get("/:userId", getProfile);
 
 module.exports = router;
